d4453d134b81165a2562dec2a29cbee6ff43501a
allseenn <allseen@live.ru>
2024-09-12 20:24:28 +0000

    04

--- 04.Lecture/server.js
+++ 04.Lecture/server.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+let uniqueID = 0;
 const articles = [];
 
-app.get('/aricles', (req, res) => {
+app.use(express.json());
+
+app.get('/articles', (req, res) => {
     res.send({articles});
+});
+
+app.get('/articles/:id', (req, res) => {
+    const article = articles.find(article => article.id === Number(req.params.id));
+
+    if(article) {
+        res.send({article});
+    } else {
+        res.status(404);
+        res.send({article: null});
+    }
+});
+
+app.post('/articles', (req, res) => {
+    uniqueID++;
+    articles.push({
+        id: uniqueID,
+        ...req.body
+    });
+    res.send({
+        id: uniqueID,
+    });
 });
 
 app.listen(port);
