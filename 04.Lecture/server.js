b1b6f158f70bb5161d0cd1f091c03d3b6866a8ad
allseenn <allseen@live.ru>
2024-09-15 13:01:23 +0000

    server

--- 04.Lecture/server.js
+++ 04.Lecture/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { checkBody, checkParams } = require('./validation/validator');
+const { idScheme, articleSchema } = require('./validation/scheme');
+
 const app = express();
 const port = 3000;
 let uniqueID = 0;
@@ -14,30 +17,50 @@ app.get('/articles/:id', (req, res) => {
     const article = articles[req.params.id-1];
     if(article)
         res.send(articles);
-    else
-        res.send({article: null});
+    else {
+    res.status(404)
+    res.send({article: null});
+}
 });
 
-app.put('/articles/:id', (req, res) => {
+app.put('/articles/:id', checkParams(idScheme), checkBody(articleSchema), (req, res) => {
+    const idValidationResult = idScheme.validate(req.params);
+    if(idValidationResult.error){
+        res.status(400);
+        res.send(idValidationResult.error);
+        return;
+    }
+
+    const articleValidationResult = articleSchema.validate(req.body);
+    if(articleValidationResult.error){
+        res.status(400);
+        res.send(articleValidationResult.error);
+        return;
+    }
+
     const article = articles[req.params.id-1];
     if(article){
         article.title = req.body.title;
         article.content = req.body.content;
-        res.send(articles);
-    }else
+        res.send(article);
+    } else {
+        res.status(404)
         res.send({article: null});
+    }
 });
 
-app.delete('/articles/:id', (req, res) => {
+app.delete('/articles/:id', checkParams(idScheme), (req, res) => {
     const article = articles[req.params.id-1];
     if(article){
         articles.splice(req.params.id-1, 1);
-        res.send(articles);
-    }else
+        res.send(article);
+    } else {
+        res.status(404)
         res.send({article: null});
+    }
 });
 
-app.post('/articles', (req, res) => {
+app.post('/articles', checkParams(idScheme), (req, res) => {
     uniqueID++;
     articles.push({
         id: uniqueID,
